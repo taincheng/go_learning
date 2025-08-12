04c1bc9ee6536ad4abe51af068d14ad34bd34e76
taincheng <zhangtc@gmail.com>
2025-08-12 18:15:14 +0800

    fix: 56

--- homework_1/leetcode56.go
+++ homework_1/leetcode56.go
@@ -1,12 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 /*
 56. 合并区间 [https://leetcode.cn/problems/merge-intervals/description/]
 */
 func merge(intervals [][]int) [][]int {
 	var res [][]int
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
 	res = append(res, intervals[0])
 	index := 0
 	for i := 1; i < len(intervals); i++ {
